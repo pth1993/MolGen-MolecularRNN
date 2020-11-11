940bd55bb09244f79b9c53e397b7332fe753f769
Erikawa <40485074+hyleih@users.noreply.github.com>
2020-11-12 05:57:10 +0900

    Add files via upload

--- Model/model.py
+++ Model/model.py
@@ -6,17 +6,20 @@ from Utils.utils import *
 
 
 class NodeRNN(nn.Module):
-    def __init__(self, input_size_adj, input_size_node, emb_size, hidden_lstm_size, num_layers, hidden_header_size,
+    def __init__(self, input_size_adj, input_size_node, emb_size, hidden_rnn_size, hidden_header_size,
                  out_features, seq_len, dr_rate=0.2):
         super(NodeRNN, self).__init__()
         self.seq_len = seq_len
         self.embedding_adj = nn.Linear(input_size_adj, emb_size, bias=False)
         self.embedding_node = nn.Embedding(input_size_node, emb_size, padding_idx=0)
-        self.lstm = nn.LSTM(input_size=emb_size, hidden_size=hidden_lstm_size, num_layers=num_layers,
-                            dropout=dr_rate, batch_first=True)
+        self.rnn1 = nn.GRU(input_size=emb_size, hidden_size=hidden_rnn_size, num_layers=1, batch_first=True)
+        self.rnn2 = nn.GRU(input_size=hidden_rnn_size, hidden_size=hidden_rnn_size, num_layers=1, batch_first=True)
+        self.rnn3 = nn.GRU(input_size=hidden_rnn_size, hidden_size=hidden_rnn_size, num_layers=1, batch_first=True)
+        self.rnn4 = nn.GRU(input_size=hidden_rnn_size, hidden_size=hidden_rnn_size, num_layers=1, batch_first=True)
         self.header = nn.Sequential(
-            nn.Linear(in_features=hidden_lstm_size, out_features=hidden_header_size),
+            nn.Linear(in_features=hidden_rnn_size, out_features=hidden_header_size),
             nn.ReLU(),
+            nn.Dropout(dr_rate),
             nn.Linear(in_features=hidden_header_size, out_features=out_features)
         )
 
@@ -29,49 +32,58 @@ class NodeRNN(nn.Module):
         h_C = self.embedding_node(x_node)
         h = torch.cat((h_S, h_C), dim=1)
         h = torch.nn.utils.rnn.pack_padded_sequence(h, x_len, batch_first=True, enforce_sorted=False)
-        h, state = self.lstm(h)
-        h, _ = torch.nn.utils.rnn.pad_packed_sequence(h, batch_first=True, total_length=self.seq_len)
-        print(h.shape, state[0].shape, state[1].shape)
-        outputs = []
+        h1, _ = self.rnn1(h)
+        h2, _ = self.rnn2(h1)
+        h3, _ = self.rnn3(h2)
+        h4, _ = self.rnn4(h3)
+        h1, _ = torch.nn.utils.rnn.pad_packed_sequence(h1, batch_first=True, total_length=self.seq_len)
+        h2, _ = torch.nn.utils.rnn.pad_packed_sequence(h2, batch_first=True, total_length=self.seq_len)
+        h3, _ = torch.nn.utils.rnn.pad_packed_sequence(h3, batch_first=True, total_length=self.seq_len)
+        h4, _ = torch.nn.utils.rnn.pad_packed_sequence(h4, batch_first=True, total_length=self.seq_len)
+        outputs = torch.zeros([h4.shape[0], self.seq_len, len(ATOM_IDX)], device=device)
         for i in range(self.seq_len):
-            y = self.header(h[:, i])
-            outputs.append(y)
-        outputs = torch.stack(outputs, dim=1)
+            y = self.header(h4[:, i])
+            outputs[:, i, ] = y
 
-        return outputs, h
+        return outputs, torch.stack([h1, h2, h3, h4], dim=1)
 
 
 class EdgeRNN(nn.Module):
-    def __init__(self, input_size, emb_size, hidden_lstm_size, num_layers, hidden_header_size, out_features,
+    def __init__(self, input_size, emb_size, hidden_rnn_size, hidden_header_size, out_features,
                  seq_len, dr_rate=0.2):
         super(EdgeRNN, self).__init__()
         self.seq_len = seq_len
         self.embedding = nn.Embedding(input_size, emb_size)
-        self.lstm = nn.LSTM(input_size=emb_size, hidden_size=hidden_lstm_size, num_layers=num_layers,
-                            dropout=dr_rate, batch_first=True)
+        self.rnn1 = nn.GRU(input_size=emb_size, hidden_size=256, num_layers=1, batch_first=True)
+        self.rnn2 = nn.GRU(input_size=256, hidden_size=hidden_rnn_size, num_layers=1, batch_first=True)
+        self.rnn3 = nn.GRU(input_size=hidden_rnn_size, hidden_size=hidden_rnn_size, num_layers=1, batch_first=True)
+        self.rnn4 = nn.GRU(input_size=hidden_rnn_size, hidden_size=hidden_rnn_size, num_layers=1, batch_first=True)
         self.header = nn.Sequential(
-            nn.Linear(in_features=hidden_lstm_size, out_features=hidden_header_size),
+            nn.Linear(in_features=hidden_rnn_size, out_features=hidden_header_size),
+            nn.Dropout(dr_rate),
             nn.ReLU(),
             nn.Linear(in_features=hidden_header_size, out_features=out_features)
         )
 
     def forward(self, x, state):
         h = self.embedding(x)
-        print(x.shape, h.shape, state[0].shape)
-        # h = torch.cat((h, h_node.view([h_node.shape[0], 1, -1])), dim=1)
-        h, state = self.lstm(h, state)
-        # outputs = []
-        # for i in range(self.seq_len):
-        #     y = self.header(h[:, i])
-        #     outputs.append(y)
-        # outputs = torch.stack(outputs, dim=1)
-        # outputs = torch.zeros([h.shape[0], ])
+        print(h.shape, state.shape)
+        h, _ = self.rnn1(h, state[:, 0].contiguous().unsqueeze(0))
+        print(h.shape)
+        h, _ = self.rnn2(h, state[:, 1].contiguous().unsqueeze(0))
+        h, _ = self.rnn3(h, state[:, 2].contiguous().unsqueeze(0))
+        h, _ = self.rnn4(h, state[:, 3].contiguous().unsqueeze(0))
 
-        return h
+        outputs = torch.zeros([h.shape[0], self.seq_len, len(BOND_IDX)], device=device)
+        for i in range(self.seq_len):
+            y = self.header(h[:, i].contiguous())
+            outputs[:, i, ] = y
+
+        return outputs
 
 
 if __name__ == "__main__":
-    model = NodeRNN(input_size_adj=12, input_size_node=9, emb_size=64, hidden_lstm_size=128, num_layers=2,
+    model = NodeRNN(input_size_adj=12, input_size_node=9, emb_size=64, hidden_rnn_size=128,
                     hidden_header_size=64, out_features=9, seq_len=40)
 
     x1 = np.ones([16, 80])
--- Model/train.py
+++ Model/train.py
@@ -10,16 +10,15 @@ from Model.dataset import MolDataset
 def train(train_list, test_list, num_epoch, batch_size, num_layers=2):
     train_dataset = MolDataset(train_list)
     test_dataset = MolDataset(test_list)
-    train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, shuffle=True, drop_last=True)
-    test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=batch_size, shuffle=True, drop_last=True)
+    train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, shuffle=False, drop_last=True)
+    test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=batch_size, shuffle=False, drop_last=True)
 
-    node_model = NodeRNN(input_size_adj=MAX_TIMESTEP, input_size_node=len(ATOM_IDX), emb_size=64, hidden_rnn_size=128,
-                         num_layers=num_layers, hidden_header_size=64, out_features=len(ATOM_IDX),
-                         seq_len=MAX_NODE).to(device)
-    edge_model = EdgeRNN(input_size=len(BOND_IDX), emb_size=128, hidden_rnn_size=128, num_layers=num_layers,
+    node_model = NodeRNN(input_size_adj=MAX_TIMESTEP, input_size_node=len(ATOM_IDX), emb_size=64, hidden_rnn_size=256,
+                         hidden_header_size=64, out_features=len(ATOM_IDX), seq_len=MAX_NODE).to(device)
+    edge_model = EdgeRNN(input_size=len(BOND_IDX), emb_size=16, hidden_rnn_size=128,
                          hidden_header_size=64, out_features=len(BOND_IDX), seq_len=MAX_TIMESTEP).to(device)
 
-    weights = torch.tensor([1.0, 10.0, 10.0, 10.0]).cuda()
+    weights = torch.tensor([1.0, 1000.0, 1000.0, 1000.0, 1.0]).cuda()
     node_criterion = nn.CrossEntropyLoss(ignore_index=0)
     edge_criterion = nn.CrossEntropyLoss(weight=weights)
 
@@ -36,18 +35,20 @@ def train(train_list, test_list, num_epoch, batch_size, num_layers=2):
             y_node = node_model(S_i, C, x_len)
 
             edge_pred = torch.zeros([batch_size, MAX_NODE*MAX_TIMESTEP, len(BOND_IDX)], requires_grad=True).to(device)
+            edge_true = torch.zeros([batch_size, MAX_NODE*MAX_TIMESTEP, ], dtype=torch.long).to(device)
             for j in range(max(x_len)):
                 edge_optimizer.zero_grad()
-
-                y_edge = edge_model(S_ij[:, j, ], y_node[1][:, :, j])
+                print(S_ij[0, j, ])
+                y_edge = edge_model(S_ij[:, j, :-1], y_node[1][:, :, j])
                 edge_pred[:, MAX_TIMESTEP*j:MAX_TIMESTEP*(j+1), ] = y_edge
+                edge_true[:, MAX_TIMESTEP * j:MAX_TIMESTEP * (j + 1), ] = S_ij[:, j, 1:]
 
-            edge_loss = edge_criterion(edge_pred.contiguous().view(-1, len(BOND_IDX)), S_ij.contiguous().view(-1, ))
+            edge_loss = edge_criterion(edge_pred.contiguous().view(-1, len(BOND_IDX)), edge_true.contiguous().view(-1, ))
             edge_loss.backward(retain_graph=True)
             edge_optimizer.step()
             print("EPOCH%d:%d, Train Edge loss:%f" % (n, i, edge_loss))
 
-            node_loss = node_criterion(y_node[0].view(-1, len(ATOM_IDX)), C.view(-1, ))
+            node_loss = node_criterion(y_node[0].view(-1, len(ATOM_IDX)), C[:, 1:].contiguous().view(-1, ))
             node_loss.backward()
             node_optimizer.step()
             print("EPOCH%d:%d, Train Node loss:%f" % (n, i, node_loss))
